a0e6fd6aeb417952d9c1c03a6ef15e64f8a7f8e5
ishmam ali <ishmamalikhan@gmail.com>
2020-04-16 10:33:19 +0500

    document object model

---
+++ js/manipulate.js
@@ -0,0 +1,12 @@
+console.log('This is tut 11');
+let a = window.document;
+// alert('hello');
+// a = prompt('This will destroy your computer. Type your name');
+
+// a = confirm('Are you sure you want to delete this page?');
+a = window.innerHeight;
+a = innerWidth;
+a = scrollY;
+a = location.toString();
+a = window.history;
+console.log(a);   
