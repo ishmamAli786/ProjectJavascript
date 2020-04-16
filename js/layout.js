58b2b0d74f01d1c5a7cfb8bacb356f6efd05f14f
ishmam ali <ishmamalikhan@gmail.com>
2020-04-16 11:22:10 +0500

    dom

---
+++ js/layout.js
@@ -0,0 +1,11 @@
+console.log('Welcome to tut 12.js');
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms[0];
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// })
+a = document.links[0];
+// use document.images and document.scripts and print the list of images and scripts on an html page
+console.log(a);
